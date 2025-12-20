b99bd882a652011bfe9715c2c07b7d3efcf4e151
mohaixiao <396367702@qq.com>
2025-12-20 21:57:50 +0800

    用户信息展示

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,15 +1,79 @@
 <template>
-  <div class="navbar">NavBar</div>
+  <div class="navbar">
+    <div class="right-menu">
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar
+            shape="square"
+            :size="40"
+            :src="authStore.userInfo?.avatar"
+          ></el-avatar>
+          <el-icon class="setting-icon"><Setting /></el-icon>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item> 首页 </el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="">
+              <el-dropdown-item>课程主页</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click="handleLogout">
+              退出登录
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import { Setting } from '@element-plus/icons-vue'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+function handleLogout() {
+  authStore.logout()
+  router.push('/login')
+}
+</script>
 
 <style lang="scss" scoped>
 .navbar {
   height: 50px;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .right-menu {
+    display: flex;
+    align-items: center;
+    float: right;
+    padding-right: 16px;
+
+    :deep(.avatar-container) {
+      cursor: pointer;
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        .el-avatar {
+          --el-avatar-background-color: none;
+          margin-right: 12px;
+        }
+        .setting-icon {
+          font-size: 16px;
+          color: #5a5e66;
+        }
+      }
+    }
+  }
 }
 </style>
